8baf9f46dd892e079f7ca6b3a68eff90dc171f61
toskrip <tomas@skripcak.net>
2021-05-11 15:57:21 +0200

    Code analysis will need custom workflow

--- radplanbio-core/src/test/java/de/dktk/dd/rpb/core/service/BlockRandomisationTest.java
+++ radplanbio-core/src/test/java/de/dktk/dd/rpb/core/service/BlockRandomisationTest.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of RadPlanBio
  *
- * Copyright (C) 2013-2018 Tomas Skripcak
+ * Copyright (C) 2013-2019 RPB Team
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,7 +39,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import static junit.framework.Assert.*;
+import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.powermock.api.mockito.PowerMockito.*;
 
@@ -62,6 +62,218 @@ public class BlockRandomisationTest {
         study.setRandomisationConfiguration(conf);
     }
 
+    @Ignore
+    @Test
+    public void simulateCR() {
+
+        // set null for ABSOLUTE (not variable block size, when min and max are the same)
+        conf.setMinimumBlockSize(3);
+        conf.setMaximumBlockSize(6);
+
+        conf.setType(BlockRandomisationConfiguration.TYPE.MULTIPLY);
+
+        Random rndb = new Random();
+
+        // Two arms
+        int numberOfTreatmentArms = 2;
+        // Planned subjects per arm (two arms)
+        int patientsPerArm1 = 44;
+        int patientsPerArm2 = 22;
+        int patientsTotal = patientsPerArm1 + patientsPerArm2;
+
+        List<TreatmentArm> arms = new ArrayList<>();
+        TreatmentArm arm1 = new TreatmentArm();
+        arm1.setName("Intervention");
+        arm1.setPlannedSubjectsCount(patientsPerArm1);
+        arms.add(arm1);
+
+        TreatmentArm arm2 = new TreatmentArm();
+        arm2.setName("Observation");
+        arm2.setPlannedSubjectsCount(patientsPerArm2);
+        arms.add(arm2);
+
+        study.setIsStratifyTrialSite(true);
+        study.setTreatmentArms(arms);
+
+        PartnerSite site1 = new PartnerSite();
+        site1.setIdentifier("DD");
+
+        List<Study> studies = new ArrayList<>();
+        studies.add(study);
+        site1.setStudies(studies);
+
+        boolean isYes = rndb.nextBoolean();
+
+        for (int i = 0; i < patientsTotal; i++) {
+            subject = new TrialSubject();
+            subject.setTrialSite(site1);
+
+            List<PrognosticVariable<?>> variables = new ArrayList<>();
+
+            // First prognostic variable definition is exclusive yes/no property
+            PrognosticVariable<String> v1 = getEmptyDichotomProperty1();
+            variables.add(v1);
+            PrognosticVariable<String> v2 = getEmptyDichotomProperty1();
+            variables.add(v2);
+            PrognosticVariable<String> v3 = getEmptyDichotomProperty1();
+            variables.add(v3);
+
+            try {
+                v1.setValue(isYes ? "yes" : "no");
+                isYes = rndb.nextBoolean();
+                v2.setValue(isYes ? "yes" : "no");
+                isYes = rndb.nextBoolean();
+                v3.setValue(isYes ? "yes" : "no");
+
+            } catch (Exception err) {
+                fail(err.getMessage());
+            }
+
+            subject.setPrognosticVariables(variables);
+
+            TreatmentArm assignedArm = study.getRandomisationConfiguration().getScheme().randomise(subject);
+
+            subject.setTreatmentArm(assignedArm);
+            assignedArm.addSubject(subject);
+
+            isYes = rndb.nextBoolean();
+        }
+
+        int v1yesv2yesv3yesIntervention = 0;
+        int v1yesv2yesv3noIntervention = 0;
+        int v1yesv2nov3yesIntervention = 0;
+        int v1yesv2nov3noIntervention = 0;
+        int v1nov2yesv3yesIntervention = 0;
+        int v1nov2yesv3noIntervention = 0;
+        int v1nov2nov3yesIntervention = 0;
+        int v1no2vnov3noIntervention = 0;
+
+        int v1yesv2yesv3yesObservation = 0;
+        int v1yesv2yesv3noObservation = 0;
+        int v1yesv2nov3yesObservation = 0;
+        int v1yesv2nov3noObservation = 0;
+        int v1nov2yesv3yesObservation = 0;
+        int v1nov2yesv3noObservation = 0;
+        int v1nov2nov3yesObservation = 0;
+        int v1no2vnov3noObservation = 0;
+
+        TreatmentArm resultArmIntervention = study.getTreatmentArms().get(0);
+        for (TrialSubject sub: resultArmIntervention.getSubjects()) {
+            if (sub.getTrialSite().getIdentifier().equals("DD")) {
+                if (sub.getTreatmentArm().getName().equals("Intervention")) {
+
+                    String v1 = (String) sub.getPrognosticVariables().get(0).getValue();
+                    String v2 = (String) sub.getPrognosticVariables().get(1).getValue();
+                    String v3 = (String) sub.getPrognosticVariables().get(2).getValue();
+
+                    // v1yesv2yesv3yesIntervention
+                    if ("yes".equals(v1) && "yes".equals(v2) && "yes".equals(v3)) {
+                        v1yesv2yesv3yesIntervention++;
+                    }
+                    // v1yesv2yesv3noIntervention
+                    else if ("yes".equals(v1) && "yes".equals(v2) && "no".equals(v3)) {
+                        v1yesv2yesv3noIntervention++;
+                    }
+                    // v1yesv2nov3yesIntervention
+                    else if ("yes".equals(v1) && "no".equals(v2) && "yes".equals(v3)) {
+                        v1yesv2nov3yesIntervention++;
+                    }
+                    // v1yesv2nov3noIntervention
+                    else if ("yes".equals(v1) && "no".equals(v2) && "no".equals(v3)) {
+                        v1yesv2nov3noIntervention++;
+                    }
+                    // v1nov2yesv3yesIntervention
+                    else if ("no".equals(v1) && "yes".equals(v2) && "yes".equals(v3)) {
+                        v1nov2yesv3yesIntervention++;
+                    }
+                    // v1nov2yesv3noIntervention
+                    else if ("no".equals(v1) && "yes".equals(v2) && "no".equals(v3)) {
+                        v1nov2yesv3noIntervention++;
+                    }
+                    // v1nov2nov3yesIntervention
+                    else if ("no".equals(v1) && "no".equals(v2) && "yes".equals(v3)) {
+                        v1nov2nov3yesIntervention++;
+                    }
+                    // v1no2vnov3noIntervention
+                    else if ("no".equals(v1) && "no".equals(v2) && "no".equals(v3)) {
+                        v1no2vnov3noIntervention++;
+                    }
+
+                }
+            }
+        }
+
+        // At least
+        assertTrue("Should be at least <" + (patientsPerArm1 - 1) + "> but was <" + resultArmIntervention.getSubjects().size() +">", resultArmIntervention.getSubjects().size() >= patientsPerArm1 - 1);
+
+        // At most
+        assertTrue("Should be at most <" + (patientsPerArm1 + 1) + "> but was <" + resultArmIntervention.getSubjects().size() + ">", resultArmIntervention.getSubjects().size() <= patientsPerArm1 + 1);
+
+        TreatmentArm resultArmObservation = study.getTreatmentArms().get(1);
+        for (TrialSubject sub: resultArmObservation.getSubjects()) {
+            if (sub.getTrialSite().getIdentifier().equals("DD")) {
+                if (sub.getTreatmentArm().getName().equals("Observation")) {
+
+                    String v1 = (String) sub.getPrognosticVariables().get(0).getValue();
+                    String v2 = (String) sub.getPrognosticVariables().get(1).getValue();
+                    String v3 = (String) sub.getPrognosticVariables().get(2).getValue();
+
+                    // v1yesv2yesv3yesObservation
+                    if ("yes".equals(v1) && "yes".equals(v2) && "yes".equals(v3)) {
+                        v1yesv2yesv3yesObservation++;
+                    }
+                    // v1yesv2yesv3noObservation
+                    else if ("yes".equals(v1) && "yes".equals(v2) && "no".equals(v3)) {
+                        v1yesv2yesv3noObservation++;
+                    }
+                    // v1yesv2nov3yesObservation
+                    else if ("yes".equals(v1) && "no".equals(v2) && "yes".equals(v3)) {
+                        v1yesv2nov3yesObservation++;
+                    }
+                    // v1yesv2nov3noObservation
+                    else if ("yes".equals(v1) && "no".equals(v2) && "no".equals(v3)) {
+                        v1yesv2nov3noObservation++;
+                    }
+                    // v1nov2yesv3yesObservation
+                    else if ("no".equals(v1) && "yes".equals(v2) && "yes".equals(v3)) {
+                        v1nov2yesv3yesObservation++;
+                    }
+                    // v1nov2yesv3noObservation
+                    else if ("no".equals(v1) && "yes".equals(v2) && "no".equals(v3)) {
+                        v1nov2yesv3noObservation++;
+                    }
+                    // v1nov2nov3yesObservation
+                    else if ("no".equals(v1) && "no".equals(v2) && "yes".equals(v3)) {
+                        v1nov2nov3yesObservation++;
+                    }
+                    // v1no2vnov3noObservation
+                    else if ("no".equals(v1) && "no".equals(v2) && "no".equals(v3)) {
+                        v1no2vnov3noObservation++;
+                    }
+                }
+            }
+        }
+
+        // At least
+        assertTrue("Should be at least <" + (patientsPerArm2 - 1) + "> but was <" + resultArmObservation.getSubjects().size() +">", resultArmObservation.getSubjects().size() >= patientsPerArm2 - 1);
+
+        // At most
+        assertTrue("Should be at most <" + (patientsPerArm2 + 1) + "> but was <" + resultArmObservation.getSubjects().size() + ">", resultArmObservation.getSubjects().size() <= patientsPerArm2 + 1);
+
+        int totalInterventionStrataGroups =  v1yesv2yesv3yesIntervention + v1yesv2yesv3noIntervention +
+                v1yesv2nov3yesIntervention + v1yesv2nov3noIntervention +
+                v1nov2yesv3yesIntervention + v1nov2yesv3noIntervention +
+                v1nov2nov3yesIntervention + v1no2vnov3noIntervention;
+
+        int totalObservationStrataGroups =  v1yesv2yesv3yesObservation + v1yesv2yesv3noObservation +
+                v1yesv2nov3yesObservation + v1yesv2nov3noObservation +
+                v1nov2yesv3yesObservation + v1nov2yesv3noObservation +
+                v1nov2nov3yesObservation +  v1no2vnov3noObservation;
+
+        assertEquals(patientsPerArm1 + patientsPerArm2, totalInterventionStrataGroups + totalObservationStrataGroups);
+    }
+
+    @Ignore
     @Test
     public void testOneSiteTwoGroupsNoStrata() {
         // Configuration
@@ -116,6 +328,7 @@ public class BlockRandomisationTest {
         }
     }
 
+    @Ignore
     @Test
     public void testOneSiteTwoGroupsNoStrataUnequalAllocations() {
         // Configuration
@@ -198,6 +411,7 @@ public class BlockRandomisationTest {
     }
 
     @Test
+    @Ignore
     public void testOneSiteTwoGroupsStrataUnequalAllocations() {
         conf.setMinimumBlockSize(3);
         conf.setMaximumBlockSize(6);
@@ -459,7 +673,7 @@ public class BlockRandomisationTest {
     }
 
 //    @Ignore
-//    public void testFourHundretAllocations2(){
+//    public void testFourHundredAllocations2(){
 //        RandomizationHelper.addArms(trial, 400, 400);
 //        conf.setMinimum(1);
 //        conf.setMaximum(2);
